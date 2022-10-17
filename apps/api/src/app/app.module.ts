5d32cfd8dafb60f2cf16428d5f058b713de4d630
Lazar <82910765+lazarks@users.noreply.github.com>
2022-10-17 14:58:17 +0200

    products module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsController } from 'libs/products/src/lib/products.controller';
+import { ProductsService } from 'libs/products/src/lib/products.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, ProductsController],
+  providers: [AppService, ProductsService],
 })
 export class AppModule {}
