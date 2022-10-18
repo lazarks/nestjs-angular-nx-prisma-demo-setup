cbdc0116adf1499ab067146b351941c0c00a8caa
Lazar <82910765+lazarks@users.noreply.github.com>
2022-10-18 11:55:45 +0200

    import environment variable (API_URL) from /libs -- setup

--- apps/ng-app/src/app/app.module.ts
+++ apps/ng-app/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { APP_CONFIG } from '@nestjs-angular-setup/app-config';
+import { environment } from '../environments/environment';
+
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { UiModule } from '@nestjs-angular-setup/ui';
@@ -8,7 +11,7 @@ import { UiModule } from '@nestjs-angular-setup/ui';
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [BrowserModule, UiModule],
-  providers: [],
+  providers: [{ provide: APP_CONFIG, useValue: environment }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- libs/ui/src/lib/products.service.ts
+++ libs/ui/src/lib/products.service.ts
@@ -1,17 +1,22 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from '@prisma/client';
+import { APP_CONFIG } from '@nestjs-angular-setup/app-config';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductsService {
-  private API_URL: string = 'http://localhost:3333/api';
+  // private API_URL: string = 'http://localhost:3333/api';
 
-  constructor(public http: HttpClient) {}
+  constructor(
+    public http: HttpClient,
+    @Inject(APP_CONFIG) private appConfig: any
+  ) {}
 
   public getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(`${this.API_URL}/products`);
+    // return this.http.get<Product[]>(`${this.API_URL}/products`);
+    return this.http.get<Product[]>(`${this.appConfig.API_URL}/products`);
   }
 }
