00695c191a3a29d54e871665e338aaac831d0c89
Lazar <82910765+lazarks@users.noreply.github.com>
2022-10-18 12:02:45 +0200

    cleanup commented lines

--- libs/ui/src/lib/products.service.ts
+++ libs/ui/src/lib/products.service.ts
@@ -8,15 +8,12 @@ import { APP_CONFIG } from '@nestjs-angular-setup/app-config';
   providedIn: 'root',
 })
 export class ProductsService {
-  // private API_URL: string = 'http://localhost:3333/api';
-
   constructor(
     public http: HttpClient,
     @Inject(APP_CONFIG) private appConfig: any
   ) {}
 
   public getProducts(): Observable<Product[]> {
-    // return this.http.get<Product[]>(`${this.API_URL}/products`);
     return this.http.get<Product[]>(`${this.appConfig.API_URL}/products`);
   }
 }
